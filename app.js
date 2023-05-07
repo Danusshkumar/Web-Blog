9cdee069b3894d6218998986db9b4f3bf896bfc9
Danusshkumar <112367768+Danusshkumar@users.noreply.github.com>
2023-05-08 01:02:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -107,6 +107,6 @@ app.get("/about",(req,res) => {
 });
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
